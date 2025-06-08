fb41abbe62610c11b3219cb86f39c8ea9af2ffe4
Bulat <sadikovba@gmail.com>
2025-06-08 23:39:58 +0300

    add order

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-import Store from "./services/Store.js";
+import proxiedStore from "./services/Store.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
+import ProductItem from "./components/ProductItem.js";
 import { OrderPage } from "./components/OrderPage.js";
+import CartItem from "./components/CartItem.js";
 
 window.app = {};
-app.store = Store;
+app.store = proxiedStore;
 app.router = Router;
 
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
   app.router.init();
+});
+
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
